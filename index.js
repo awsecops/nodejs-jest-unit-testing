6f5852d1e94156cc7a5881049f1df6a34396ef16
awsecops <96880169+awsecops@users.noreply.github.com>
2022-07-19 21:38:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const router = require('./routes');
 const app = express();
 app.use(express.json());
 app.use(router);
-app.listen(8080, () => console.log('server listening on port 8080'));
+app.listen(8086, () => console.log('server listening on port 8086'));
